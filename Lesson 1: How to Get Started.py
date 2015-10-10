4096d0781ff61b6ab2732aa3961aada9bc3e6709
Amor Dimaano <amor.dimaano@obf.ateneo.edu>
2015-10-10 15:07:21 +0800

    Lesson 1: Speed of Light
    2nd quiz in Lesson 1

---
+++ Lesson 1: How to Get Started.py
@@ -0,0 +1,16 @@
+__author__ = 'amordimaano'
+
+
+# First Programming Quiz
+# Write a Python program that prints out the number of minutes in seven weeks.
+
+def minutes_in_weeks():
+    minutes_in_hour = 60
+    hours_in_day = 24
+    days_in_week = 7
+    print minutes_in_hour * hours_in_day * days_in_week * 7
+
+minutes_in_weeks()
+
+
+
